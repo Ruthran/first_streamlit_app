5f371633327dcc576caf391124003276f86ded96
Ruthran <ruthrangee@gmail.com>
2023-07-01 07:32:48 +0530

    Update streamlit_app.py
    With Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
+import pandas
 streamlit.title('Healthy Dinner')
 streamlit.header('Breakfast')
 streamlit.text('🥣 One cup of mile')
 streamlit.text('🐔 Egg Omlet')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
