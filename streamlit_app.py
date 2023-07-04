bc21c46ddc80c988c6d16f6b9ed554eaac74759f
Ruthran <ruthrangee@gmail.com>
2023-07-04 09:42:46 +0530

    Update streamlit_app.py
    json

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,6 @@ streamlit.dataframe(fruits_to_show)
 #Full List
 streamlit.dataframe(my_fruit_list)
 
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
