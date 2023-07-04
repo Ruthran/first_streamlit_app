a9c1a9f62ca3adbe6a576eee2a8e94bf9d0a46fb
Ruthran <ruthrangee@gmail.com>
2023-07-04 14:08:10 +0530

    Update streamlit_app.py
    Try-Except

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,18 +21,22 @@ streamlit.dataframe(fruits_to_show)
 streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get the information");
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Normalizing Json data with Pandas
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # creating dataframe with json data
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  Streamlit.error()
+  
+#  streamlit.write('The user entered ', fruit_choice)
+#  streamlit.text(fruityvice_response.json())
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# streamlit.text(fruityvice_response.json())
-
-# Normalizing Json data with Pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# creating dataframe with json data
-streamlit.dataframe(fruityvice_normalized)
 streamlit.stop();
 
 #Snowflake Connection
